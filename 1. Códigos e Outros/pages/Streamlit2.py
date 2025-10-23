8a13c9b1a762091acbbe4dae3a0f9148e88586af
kautz-collioni <kautz.collioni@gmail.com>
2025-10-23 15:11:08 -0300

    Update Streamlit2.py

--- 1. Códigos e Outros/pages/Streamlit2.py
+++ 1. Códigos e Outros/pages/Streamlit2.py
@@ -3,7 +3,7 @@ import base64
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import Conector as con
 
 # ========================= 1. PAGE CONFIGURATION AND STYLE LOADING =========================
